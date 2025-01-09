78a8ab2990b803ed896b6ff0ebe36a962c48a354
Roman M Groblicki <namor5772@gmail.com>
2025-01-09 12:14:49 +1100

    commit6

--- Radio4.py
+++ Radio4.py
@@ -14,12 +14,6 @@ aStation = [
     ["SMOOTH953","Smooth FM Sydney 95.3","https://playerservices.streamtheworld.com/api/livestream-redirect/SMOOTH953.mp3"]
 ]
 
-
-# Replace with your radio station's streaming URL
-#stream_url = 'https://live-radio01.mediahubaustralia.com/2LRW/mp3/'
-#stream_url = 'https://live-radio01.mediahubaustralia.com/2FMW/mp3/'
-#stream_url = 'https://playerservices.streamtheworld.com/api/livestream-redirect/ARN_WSFM.mp3'
-#stream_url = 'https://playerservices.streamtheworld.com/api/livestream-redirect/SMOOTH953.mp3'
 vlc_path = "C:\\Program Files\\VideoLAN\\VLC\\vlc.exe"
 
 # Start streaming using cvlc
